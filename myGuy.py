1304871f4cbed0c8505f6cba80985b6d00c7f9c8
Uchenna Okehie <dizzybones007@yahoo.com>
2023-03-04 02:13:52 +0000

    -all

---
+++ myGuy.py
@@ -0,0 +1,199 @@
+#trying to rekindle my phyton life
+
+name = 'my name is uchenna'
+
+print(name)
+
+#trying to rekindle my phyton life
+
+name = 'my name is uchenna'
+
+print(name)
+
+#trying to rekindle my phyton life
+
+name = 'my name is uchenna'
+
+print(name)
+
+#trying to rekindle my phyton life
+
+name = 'my name is uchenna'
+
+print(name)
+
+#trying to rekindle my phyton life
+
+name = 'my name is uchenna'
+
+print(name)
+
+#trying to rekindle my phyton life
+
+name = 'my name is uchenna'
+
+print(name)
+
+#trying to rekindle my phyton life
+
+name = 'my name is uchenna'
+
+print(name)
+
+#trying to rekindle my phyton life
+
+name = 'my name is uchenna'
+
+print(name)
+
+#trying to rekindle my phyton life
+
+name = 'my name is uchenna'
+
+print(name)
+
+#trying to rekindle my phyton life
+
+name = 'my name is uchenna'
+
+print(name)
+
+#trying to rekindle my phyton life
+
+name = 'my name is uchenna'
+
+print(name)
+
+#trying to rekindle my phyton life
+
+name = 'my name is uchenna'
+
+print(name)
+
+#trying to rekindle my phyton life
+
+name = 'my name is uchenna'
+
+print(name)
+
+#trying to rekindle my phyton life
+
+name = 'my name is uchenna'
+
+print(name)
+
+#trying to rekindle my phyton life
+
+name = 'my name is uchenna'
+
+print(name)
+
+#trying to rekindle my phyton life
+
+name = 'my name is uchenna'
+
+print(name)
+
+#trying to rekindle my phyton life
+
+name = 'my name is uchenna'
+
+print(name)
+
+#trying to rekindle my phyton life
+
+name = 'my name is uchenna'
+
+print(name)
+
+#trying to rekindle my phyton life
+
+name = 'my name is uchenna'
+
+print(name)
+
+#trying to rekindle my phyton life
+
+name = 'my name is uchenna'
+
+print(name)
+
+#trying to rekindle my phyton life
+
+name = 'my name is uchenna'
+
+print(name)
+
+#trying to rekindle my phyton life
+
+name = 'my name is uchenna'
+
+print(name)
+
+#trying to rekindle my phyton life
+
+name = 'my name is uchenna'
+
+print(name)
+
+#trying to rekindle my phyton life
+
+name = 'my name is uchenna'
+
+print(name)
+
+#trying to rekindle my phyton life
+
+name = 'my name is uchenna'
+
+print(name)
+
+#trying to rekindle my phyton life
+
+name = 'my name is uchenna'
+
+print(name)
+
+#trying to rekindle my phyton life
+
+name = 'my name is uchenna'
+
+print(name)
+
+#trying to rekindle my phyton life
+
+name = 'my name is uchenna'
+
+print(name)
+
+#trying to rekindle my phyton life
+
+name = 'my name is uchenna'
+
+print(name)
+
+#trying to rekindle my phyton life
+
+name = 'my name is uchenna'
+
+print(name)
+
+#trying to rekindle my phyton life
+
+name = 'my name is uchenna'
+
+print(name)
+
+#trying to rekindle my phyton life
+
+name = 'my name is uchenna'
+
+print(name)
+
+#trying to rekindle my phyton life
+
+name = 'my name is uchenna'
+
+print(name)
+
+
